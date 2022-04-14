c6d57967ab5bbb4c6ed623f1314ef7a94ea1b52c
robycigar <rabihutomo11@gmail.com>
2022-04-14 17:15:53 +0700

    fix: login controller

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,7 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -44,25 +44,25 @@ class User extends Authenticatable
     ];
 
 
-    // // Rest omitted for brevity
+    // Rest omitted for brevity
 
-    // /**
-    //  * Get the identifier that will be stored in the subject claim of the JWT.
-    //  *
-    //  * @return mixed
-    //  */
-    // public function getJWTIdentifier()
-    // {
-    //     return $this->getKey();
-    // }
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
 
-    // /**
-    //  * Return a key value array, containing any custom claims to be added to the JWT.
-    //  *
-    //  * @return array
-    //  */
-    // public function getJWTCustomClaims()
-    // {
-    //     return [];
-    // }
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
